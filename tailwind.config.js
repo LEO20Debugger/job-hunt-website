ce41e62b2a9ab9984c3d4a526ad010bb695b405b
Brainergybyleo <leonard6oba@gmail.com>
2024-09-04 19:59:37 +0100

    added Navbar & Hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Roboto', 'sans-serif']
+        sans: ['Roboto', 'sans-serif'],
       },
       gridTemplateColumns: {
-        '70-30': '70% 28%',
+        '70/30': '70% 28%',
       },
     },
   },

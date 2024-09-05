adfb3eacf86d0574c6c92fe24044f75737570d32
Brainergybyleo <leonard6oba@gmail.com>
2024-09-05 15:05:16 +0100

    updated src folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
-import Hero from '../components/Hero'
-import HomeCards from '../components/HomeCards'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import HomeCards from './components/HomeCards'
 
 const App = () => {
   return (
